4354909e13270ec20fe9bf83e25dc5c6d0cc4cfe
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-10 14:30:18 +0200

    feat: add movies with genre in page

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,21 @@ function App() {
   return (
     <>
       <h1>Movie Filter</h1>
-      <h2>Cerca Film</h2>
-      <label>Cerca per categoria</label>
-      <select value={searchCategory} onChange={e => setSearchCategory(e.target.value)}>
-        <option value="">---</option>
-        <option>Fantascienza</option>
-        <option>Thriller</option>
-        <option>Romantico</option>
-        <option>Azione</option>
-      </select>
+      <section>
+        <h2>Cerca Film</h2>
+        <label>Cerca per categoria</label>
+        <select value={searchCategory} onChange={e => setSearchCategory(e.target.value)}>
+          <option value="">---</option>
+          <option>Fantascienza</option>
+          <option>Thriller</option>
+          <option>Romantico</option>
+          <option>Azione</option>
+        </select>
+      </section>
+      {movies.map((movie, index) => <article key={index}>
+        <h3>{movie.title}</h3>
+        <p>Genere: <strong>{movie.genre}</strong></p>
+      </article>)}
     </>
   );
 }
